e861e06120335859df0e4197a49ebb6db038c3a1
Eric Le <eriiicle@outlook.com>
2025-09-06 16:23:50 +0200

    feat(game): display winner and choices

--- script.js
+++ script.js
@@ -8,49 +8,116 @@ const rockBtn = document.querySelector('#rock-btn');
 const paperBtn = document.querySelector('#paper-btn');
 const scissorsBtn = document.querySelector('#scissors-btn');
 
+// Player choice display
+let playerChoiceDisplay = document.querySelector('#player-choice');
+let computerChoiceDisplay = document.querySelector('#computer-choice');
+
 // Winner display container
-let winnerDisplay = document.querySelector('winner');
+let winnerDisplay = document.querySelector('.winner');
 
 // Player score display
-let playerScoreDisplay = document.querySelector('player-score');
-let computerScoreDisplay = document.querySelector('computer-score');
+let playerScoreDisplay = document.querySelector('.player-score');
+let computerScoreDisplay = document.querySelector('.computer-score');
 
 // Get choices
 function getComputerChoice() {
   let randomNumber = Math.floor(3 * Math.random());
-  switch(randomNumber) {
+  switch (randomNumber) {
     case 0:
       return 'rock';
     case 1:
       return 'paper';
     case 2:
-      return 'scissors'
+      return 'scissors';
   }
 }
 
-function getWinnerChoice(humanChoice, computerChoice) {
+function insertEmoji(choice) {
+  switch (choice) {
+    case 'rock':
+      return 'rock 🗿';
+    case 'paper':
+      return 'paper 📄';
+    case 'scissors':
+      return '✂️';
+    default:
+      return choice;
+  }
+}
+// Update choice display
+function updateChoices(humanChoice, computerChoice) {
+  playerChoiceDisplay.textContent = insertEmoji(humanChoice);
+  computerChoiceDisplay.textContent = insertEmoji(computerChoice);
+}
+
+// Determines round winner: true = player wins, false = computer wins, undefined = draw
+function getWinner(humanChoice, computerChoice) {
   let totalLength = humanChoice.length + computerChoice.length;
   switch (totalLength) {
     case 9:
-      return 'paper';
+      return humanChoice === 'paper';
     case 12:
-      return 'rock';
+      return humanChoice === 'rock';
     case 13:
-      return 'scissors';
+      return humanChoice === 'scissors';
     default:
-      return 'invalid choice';
+      return undefined;
   }
 }
 
-// Helper functions
-function capitalizeFirstLetter(word) {
-  return word.charAt(0).toUpperCase() + word.slice(1);
+function displayWinner(winner) {
+  let message;
+  let bgColor;
+
+  switch (winner) {
+    case true:
+      message = 'You win!';
+      bgColor = '#43c25a';
+      break;
+    case false:
+      message = 'Compueter wins!';
+      bgColor = '#f85454';
+      break;
+    case undefined:
+      message = `It's a tie!`;
+      bgColor = '#f9b50f';
+  }
+
+  updateWinner(message, bgColor);
 }
 
-function playRound(humanChoice) {
-  console.log(`Human choice: ${humanChoice}`);
+function updateWinner(message, bgColor) {
+  let existingParagraph = winnerDisplay.querySelector('p');
+
+  if (existingParagraph) {
+    // Update text and color
+    existingParagraph.textContent = message;
+    existingParagraph.style.cssText = `background: ${bgColor};`;
+  } else {
+    // Create a new paragraph
+    let newParagraph = document.createElement('p');
+    newParagraph.textContent = message;
+    newParagraph.style.cssText = `background: ${bgColor};`;
+    winnerDisplay.appendChild(newParagraph);
+  }
 }
-// Events 
+
+// Main play round
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
+  console.log('');
+  console.log(`Human choice: ${humanChoice}`);
+  console.log(`Computer choice: ${computerChoice}`);
+  updateChoices(humanChoice, computerChoice);
+
+  console.log('');
+  let winner = getWinner(humanChoice, computerChoice);
+  console.log(`Human won?: ${winner}`);
+
+  console.log('');
+  displayWinner(winner);
+}
+// Events
 rockBtn.addEventListener('click', () => {
   playRound('rock');
 });
